987c7114bea6febf42c642b194edd57c19028359
Ayush <Ayushsri2011@gmail.com>
2018-09-11 01:06:41 +0530

    Minor change

--- app/src/main/java/com/nightcrawler/popularmovies/stage2.java
+++ app/src/main/java/com/nightcrawler/popularmovies/stage2.java
@@ -2,13 +2,17 @@ package com.nightcrawler.popularmovies;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ProgressBar;
+
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
@@ -16,20 +20,26 @@ import java.util.concurrent.ExecutionException;
 import static com.nightcrawler.popularmovies.CommonUtils.generateAddress;
 
 public class stage2 extends AppCompatActivity {
-//    private static final String TAG = MainActivity.class.getSimpleName();
+    //    private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recyclerView;
     private CustomAdapter adapter;
-    private Button button_next;    private Button button_prev;
-    private int search_type,page,total_pages=0;
+    //    private Button button_next;    private Button button_prev;
+//    ProgressBar pb;
+    private int search_type, page, total_pages = 0;
     private ArrayList<CustomPojo> movieList;
+    private ArrayList<CustomPojo> nmovieList;
     private ProgressBar pb2;
     private String category;
+    Boolean isScrolling = false;
+
+    int currentItems, totalItems, scrollOutItems,test=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pb2= findViewById(R.id.refreshdata_progress);
+        pb2 = findViewById(R.id.refreshdata_progress);
         pb2.setVisibility(View.INVISIBLE);
 
 
@@ -37,106 +47,96 @@ public class stage2 extends AppCompatActivity {
         Intent intent = getIntent();
 
         Bundle args = intent.getBundleExtra("BUNDLE");
-        movieList = (ArrayList<CustomPojo>)args.getSerializable("ARRAYLIST");
-        recyclerView= findViewById(R.id.bohe);
+        movieList = (ArrayList<CustomPojo>) args.getSerializable("ARRAYLIST");
+
+        recyclerView = findViewById(R.id.bohe);
 //        picture = findViewById(R.id.picture);
+        final GridLayoutManager lm = new GridLayoutManager(stage2.this, 2);
+        recyclerView.setLayoutManager(lm);
+//        recyclerView.setLayoutManager(new GridLayoutManager(stage2.this, 2));
+        adapter = new CustomAdapter(this);
+        adapter.setListContent(movieList);
+        recyclerView.setAdapter(adapter);
+//        populateRecyclerViewValues(movieList);
 
-        recyclerView.setLayoutManager(new GridLayoutManager(stage2.this, 2));
-        adapter=new CustomAdapter(this);
-        populateRecyclerViewValues(movieList);
+        total_pages = args.getInt("total_pages");
+        search_type = args.getInt("search_type");
+        category = args.getString("category", "movie");
 
-        total_pages=args.getInt("total_pages");
-        search_type=args.getInt("search_type");
-        category=args.getString("category","movie");
-
-        page=args.getInt("page");
-        button_next= findViewById(R.id.next_button);
-        button_prev= findViewById(R.id.prev_button);
+        page = args.getInt("page");
+//        button_next= findViewById(R.id.next_button);
+//        button_prev= findViewById(R.id.prev_button);
 
 
-        button_prev.setOnClickListener(new View.OnClickListener() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
-            public void onClick(View v) {
-                pb2.setVisibility(View.VISIBLE);
-                if(!CommonUtils.checkConnectivity(stage2.this)) {
-                    CommonUtils.alert(stage2.this);
-                }
-                else {
-                    if (page == 1) {
-                        CommonUtils.alert2(stage2.this);
-                    } else {
-                        page--;
-                        String address_temp;
-                        if (search_type == 0)
-                            address_temp = CommonUtils.generateAddress(stage2.this,getString(R.string.REQUEST_TOPRATED), page,category);
-                        else
-                            address_temp = CommonUtils.generateAddress(stage2.this,getString(R.string.REQUEST_POPULAR), page,category);
-
-                        Query q = new Query();
-
-                        try {
-                            q.execute(address_temp).get();
-                        } catch (InterruptedException | ExecutionException e) {
-                            e.printStackTrace();
-                        }
-
-                        jsonProcessing c = new jsonProcessing(q.local_response,category);
-                        movieList = c.process();
-                        populateRecyclerViewValues(movieList);
-                    }
-
-                }
-                pb2.setVisibility(View.INVISIBLE);
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
+                    isScrolling = true;
             }
-        });
 
-
-
-
-        button_next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                pb2.setVisibility(View.VISIBLE);
-                if(!CommonUtils.checkConnectivity(stage2.this)) {
-
-                    CommonUtils.alert(stage2.this);
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                currentItems = lm.getChildCount();
+                totalItems = lm.getItemCount();
+                scrollOutItems = lm.findFirstVisibleItemPosition();
+                if (isScrolling && (currentItems + scrollOutItems == totalItems)) {
+                    pb2.setVisibility(View.VISIBLE);
+                    fetchData();
                 }
-                else
-                {
-                    if(page==total_pages)
-                    {
-                        CommonUtils.alert3(stage2.this);
-                    }
-                    else
-                    {
-                        page++;
-                        String ty;
-                        if(search_type==0)
-                            ty="top_rated";
-                        else
-                            ty="popular";
-
-                        String address_temp = generateAddress(stage2.this,ty,page,category);
-                        Query q=new Query();
-
-                        try {
-                            q.execute(address_temp).get();
-                        } catch (InterruptedException | ExecutionException e) {
-                            e.printStackTrace();
-                        }
-                        jsonProcessing c=new jsonProcessing(q.local_response,category);
-                        movieList=c.process();
-                        populateRecyclerViewValues(movieList);
-                    }}
-                pb2.setVisibility(View.INVISIBLE);
             }
         });
     }
 
-    private void populateRecyclerViewValues(ArrayList<CustomPojo> movieList) {
 
-        adapter.setListContent(movieList);
-        recyclerView.setAdapter(adapter);
+    public void fetchData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (page == total_pages) {
+                    CommonUtils.alert3(stage2.this);
+                } else {
+                    page++;
+                    String ty;
+                    if (search_type == 0)
+                        ty = "top_rated";
+                    else
+                        ty = "popular";
+
+                    String address_temp = generateAddress(stage2.this, ty, page, category);
+                    Query q = new Query();
+
+                    try {
+                        q.execute(address_temp).get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                    jsonProcessing c = new jsonProcessing(q.local_response, category);
+
+                    if (test!=0)
+                        nmovieList.clear();
+                    nmovieList = c.process();
+                    test=1;
+                    populateRecyclerViewValues(nmovieList);
+                    pb2.setVisibility(View.INVISIBLE);
+                }
+            }
+        }, 3000);
+
+
+    }
+
+    private void populateRecyclerViewValues(ArrayList<CustomPojo> nmovieList) {
+//        Log.d("test", "Size is-- " + movieList.size());
+//        Log.d("test", "Size is-- " + nmovieList.size());
+        movieList.addAll(nmovieList);
+//        adapter.setListContent(movieList);
+        //        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -148,7 +148,6 @@ public class stage2 extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
 
 
 }
--- app/src/main/java/com/nightcrawler/popularmovies/stage3.java
+++ app/src/main/java/com/nightcrawler/popularmovies/stage3.java
@@ -11,11 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -241,7 +243,7 @@ public class stage3 extends AppCompatActivity {
     ArrayAdapter<String> adp = new ArrayAdapter<>(getBaseContext(), R.layout.list, li);
     list.setAdapter(adp);
     list.setClickable(false);
-
+    setListViewHeightBasedOnChildren(list);
 }
 
     public void loadVideos()
@@ -279,7 +281,7 @@ public class stage3 extends AppCompatActivity {
             }
 
         });
-
+        setListViewHeightBasedOnChildren(list2);
 
     }
 
@@ -293,19 +295,29 @@ public class stage3 extends AppCompatActivity {
         }
     }
 
+    /**** Method for Setting the Height of the ListView dynamically.
+     **** Hack to fix the issue of not showing all the items of the ListView
+     **** when placed inside a ScrollView  ****/
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
 
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
 
-
-
-
-
-
-
-
-
-
-
-
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }
 }
 
 
